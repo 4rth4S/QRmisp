460c3cb3e9bf021b534a8c2eab6495a7bfffab59
nico brambi <nbrambilla@sadvisor.com>
2021-03-09 12:24:30 -0300

    adecuacion a entorno de lab

--- master_qradar_misp_ipdst.py
+++ master_qradar_misp_ipdst.py
@@ -6,11 +6,22 @@ import pymisp
 import json
 requests.packages.urllib3.disable_warnings()
 
-url_pull = 'https://<misp_fqdn>/attributes/restSearch/json'
-headers_pull = {'Authorization': '<misp_token>', 'Content-Type': 'application/json', 'Accept': 'application/json'}
+###########################
+
+misp_fqdn = "192.168.32.65"
+misp_token = "l030qkovNgklQjSQ8Str4FMEbSBks3T6SDy9RGH0"
+qradar_fqdn = "192.168.32.154"
+qradar_token = "1fc3ea23-5a86-4439-b867-481e582b1f37"
+reference_set_name = "_MISP_Event_IOC_URLS"
+
+########################################################################
+
+
+url_pull = 'https://'+misp_fqdn+'/attributes/restSearch/json'
+headers_pull = {'Authorization': misp_token , 'Content-Type': 'application/json', 'Accept': 'application/json'}
 data_pull = {"request": {"type": "ip-dst", "category": "Network activity", "last": "1d", "enforceWarnlinglist": "True"}}
-url_push = 'https://<qradar_fqdn>/api/reference_data/sets/bulk_load/<reference_set_name>'
-headers_push = {'SEC': '<token>', 'Content-Type': 'application/json', 'Version': '9.0', 'Accept': 'application/json'}
+url_push = 'https://'+qradar_fqdn+'/api/reference_data/sets/bulk_load/'+reference_set_name
+headers_push = {'SEC': qradar_token , 'Content-Type': 'application/json', 'Version': '9.0', 'Accept': 'application/json'}
 
 # Pull the list of attributes from TIA-REPO
 r = requests.post(url_pull, headers=headers_pull, json=data_pull, verify=False)

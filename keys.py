9c0a5ff6b4d93f95ee37f9d1c6fd7b252851b7fc
Arthas <nicolas192345@gmail.com>
2021-03-15 23:46:06 -0300

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,5 @@
+misp_verifycert = False
+misp_fqdn = "10.10.10.101" #Acá va una IP o FQDN para poder armar la request hacia el MISP
+misp_token = "l030qkovNG2lqjiN8Str4FMEbSBks3T6SDy9Rgh0" #Acá va el token de MISP que se le brinda al usuario para automatizaciones.
+qradar_fqdn = "10.10.10.102" #Acá va una IP o FQDN para poder armar el post hacia QRadar
+qradar_token = "1fc3ea23-2a46-4439-s867-481q581b1f37" #Acá va el token generado en Authorized Applications

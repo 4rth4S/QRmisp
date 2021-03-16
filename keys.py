44c1e704ee751bcf5e4435612bb31ceac38e8ccd
Arthas <nicolas192345@gmail.com>
2021-03-16 00:02:51 -0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
 misp_verifycert = False
-misp_fqdn = "10.10.10.101" #Acá va una IP o FQDN para poder armar la request hacia el MISP
-misp_token = "l030qkovNG2lqjiN8Str4FMEbSBks3T6SDy9Rgh0" #Acá va el token de MISP que se le brinda al usuario para automatizaciones.
-qradar_fqdn = "10.10.10.102" #Acá va una IP o FQDN para poder armar el post hacia QRadar
-qradar_token = "1fc3ea23-2a46-4439-s867-481q581b1f37" #Acá va el token generado en Authorized Applications
+misp_fqdn = "10.10.10.101" #Acá va la IP o FQDN del nodo MISP
+misp_token = "l030qkovNG2lqjiN8Str4FMEbSBks3T6SDy9Rgh0" #Acá va el token de MISP que le brinda al usuario para hacer automatizaciones.
+qradar_fqdn = "10.10.10.102" #Acá va la IP o FQDN de QRadar
+qradar_token = "1fc3ea23-2a46-4439-s867-481q581b1f37" #Acá va el token generado en Authorized Applications en QRadar

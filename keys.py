6011caa63841637100359e60fd70a5eeb737cf08
Arthas <nicolas192345@gmail.com>
2021-04-21 13:46:20 -0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -3,7 +3,7 @@
 misp_verifycert = False
 
 
-misp_fqdn = "192.168.32.65"
-misp_token = "l030qkovNgklQjSQ8Str4FMEbSBks3T6SDy9RGH0"
-qradar_fqdn = "192.168.32.154"
-qradar_token = "1fc3ea23-5a86-4439-b867-481e582b1f37"
+misp_fqdn = "10.10.10.3"
+misp_token = "l040kqvoNkglQjSQ8Str4FMEbSBks3T6SDy9RGH0"
+qradar_fqdn = "10.10.10.2"
+qradar_token = "1fc3sd23-5a86-4439-b867-481e582c1a37"
